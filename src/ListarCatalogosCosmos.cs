0c2d262c44ca77f568bf7dce74437a923fa9a9d5
GabrielGuirro <76258211+GabrielGuirro@users.noreply.github.com>
2025-01-22 11:09:50 -0300

    Create ListarCatalogosCosmos.cs
    Adicionando a função ListarCatalogosCosmos.cs para listar todos os catálogos armazenados no Cosmos DB

---
+++ src/ListarCatalogosCosmos.cs
@@ -0,0 +1,28 @@
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class ListarCatalogosCosmos
+{
+    [FunctionName("ListarCatalogosCosmos")]
+    public static async Task Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
+        [CosmosDB(
+            databaseName: "NetflixCatalog",
+            collectionName: "Catalogos",
+            ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
+        ILogger log)
+    {
+        var collectionUri = UriFactory.CreateDocumentCollectionUri("NetflixCatalog", "Catalogos");
+
+        var query = client.CreateDocumentQuery<dynamic>(collectionUri)
+                          .AsEnumerable();
+
+        var result = query.ToList();
+
+        return new OkObjectResult(result);
+    }
+}
